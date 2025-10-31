8c44cdfc67b13bfa45afea81394fe2e64e93c5a6
hughfabre <me@hughfabre.dev>
2025-11-01 07:19:01 +0900

    feat: Add footer

---
+++ components/organisms/footer.tsx
@@ -0,0 +1,18 @@
+export function Footer() {
+  return (
+    <footer className="max-w-2xl mx-auto px-4 py-6">
+      <p className="text-sm footer-text">
+        <a 
+          href="https://mit-license.org/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          MIT License
+        </a>
+        {' '}|{' '}2025-PRESENT © Hugh Fabre
+      </p>
+    </footer>
+  );
+}
+
